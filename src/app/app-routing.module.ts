99a994d1a5bd72a922c3dea5e1cee00d2b1cd4e7
retlok <11900551@student.pxl.be>
2022-04-30 18:52:34 +0200

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HuaweiComponent } from './components/huawei/huawei.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "huawei", component: HuaweiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
